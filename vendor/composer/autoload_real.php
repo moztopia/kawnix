07d6abe63df0e0e63b0caf0e5a57ecddfd237076
mozrin <mozrin@gmail.com>
2025-05-07 11:18:16 +0000

    ready for packagelist

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitc7c1c9149828b74eed1251c87b7ae7de
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitc7c1c9149828b74eed1251c87b7ae7de', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitc7c1c9149828b74eed1251c87b7ae7de', 'loadClassLoader'));
