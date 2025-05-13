60634f27c2924c9157e6220c2022241e96236361
mozrin <mozrin@gmail.com>
2025-05-13 19:06:39 +0700

    Prepare for 0.1.3-dev development

--- src/Setup.php
+++ src/Setup.php
@@ -1,13 +1,11 @@
 <?php
 
-namespace Kawnix;
+namespace Kawnix\Core;
 
 class Setup
 {
     public static function install()
     {
-        // Convert to using a create-project soon
-        //
-        // For now, after installation ... copy the src\save to the root of the project.
+        echo "Kawnix setup executed!";
     }
 }
